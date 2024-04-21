98a51ce5fd1bb35669622c9882cd3684bdb90742
chandni thakkar <chandnithakkar2908@gmail.com>
2024-04-21 23:57:20 +0530

    initial commit

---
+++ ecomsite/shop/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from.models import Products
+from django.core.paginator import Paginator
+# Create your views here.
+
+
+def index(request):
+    #search code
+    product_object= Products.objects.all()
+    item_name = request.GET.get('item_name')
+    if item_name !="" and item_name is not None:
+        product_object=product_object.filter(title__icontains= item_name)
+
+    #paginator code
+
+    paginator =Paginator(product_object,2)
+    page= request.GET.get('page')
+    product_object = paginator.get_page(page)
+    return render(request,'shop/index.html',{'product_object':product_object })
+
+def detail(request,id):
+    product_object =Products.objects.get(id=id)
+    return render(request,'shop/detail.html',{'product_object':product_object})
+
+def checkout(request):
+
+    return render(request,'shop/checkout.html')
+
